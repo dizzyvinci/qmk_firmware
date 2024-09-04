6ac7080d4cdde26d0b3b4e875c4c44d597c902f3
dizzyvinci <dizzyvinci@gmail.com>
2024-09-04 15:21:57 -0400

    qmkconfigged

--- keyboards/vitamins_included/keymaps/dizzyvinci_vial/config.h
+++ keyboards/vitamins_included/keymaps/dizzyvinci_vial/config.h
@@ -34,3 +34,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                                 SONG(COLEMAK_SOUND) \
                               }
 #endif
+
+#define MIDI_ADVANCED
+
+/* disable debug print */
+// #define NO_DEBUG
+
+/* disable print */
+// #define NO_PRINT
+
+/* disable action features */
+#define NO_ACTION_LAYER
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
+
---
+++ keyboards/vitamins_included/keymaps/dizzyvinci_vial/rules.mk
@@ -0,0 +1,9 @@
+LTO_ENABLE = yes
+#VIA_ENABLE = yes
+VIAL_ENABLE = yes
+#MAGIC_ENABLE = yes
+#MOUSEKEY_ENABLE = yes
+MIDI_ENABLE = yes
+# HAPTIC_ENABLE = yes
+# Disable unsupported hardware
+BACKLIGHT_SUPPORTED = no
